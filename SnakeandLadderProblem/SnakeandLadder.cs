d80d5b346e9fe2b71fc68beaa62205530e8ac472
Milan Biranwar <milanbiranwar06@gmail.com>
2022-07-31 17:12:55 +0530

    Repeating game till the player reaches winning position

--- SnakeandLadderProblem/SnakeandLadder.cs
+++ SnakeandLadderProblem/SnakeandLadder.cs
@@ -34,12 +34,14 @@ namespace SnakeandLadderProblem
 
         }
 
-
+        public static int dicecount = 0;
+        public static int num1;
+        public static int position = 0;
         public static void SnakeandLadderUC3()
         {
-            int dicecount = 0;
-            int num1;
-            int position = 0;
+            //int dicecount = 0;
+            //int num1;
+            //int position = 0;
 
             //Random number for rolldice
             Random obj = new Random();
@@ -72,6 +74,62 @@ namespace SnakeandLadderProblem
 
 
                 position = position - rollDice;
+                Console.WriteLine("You got snake \n your position: {0} \n ", position);
+
+            }
+
+        }
+
+        
+        public static void SankeandLadderUC4()
+        {
+            Random obj = new Random();
+            int rollDice = obj.Next(1, 7);
+            Console.WriteLine("Dice Value: " + rollDice);
+
+            Random num = new Random();
+            int value = num.Next(1, 4);
+            Console.WriteLine("case number is " + value);
+
+            if (value == 1)
+            {
+
+                Console.WriteLine("You got NO PLAY Option");
+
+            }
+            else if (value == 2)
+            {
+                Console.WriteLine("You got Ladder Option");
+
+                position = position + rollDice;
+
+                if (position > 100)
+                {
+
+                    position = position - rollDice;
+                    Console.WriteLine("You got Ladder");
+
+
+                }
+
+                Console.WriteLine(" your position: {0}", position);
+
+            }
+            else
+            {
+                position = position - rollDice;
+
+                Console.WriteLine("You got Snake Option");
+
+                if (position < 0)
+                {
+
+                    position = 0;
+
+                    Console.WriteLine("Starting game  again");
+
+
+                }
                 Console.WriteLine("You got snake \n your position: {0} \n ", position);
 
             }
