19760ad992edcd314b8d317ffe9e4ba84f16ab83
Milan Biranwar <milanbiranwar06@gmail.com>
2022-07-31 17:40:17 +0530

    Ensuring the player gets exact winning position

--- SnakeandLadderProblem/SnakeandLadder.cs
+++ SnakeandLadderProblem/SnakeandLadder.cs
@@ -136,7 +136,59 @@ namespace SnakeandLadderProblem
 
         }
 
-       
+        public static void SnakeandLadderUC5()
+        {
+            Random obj = new Random();
+            int rollDice = obj.Next(1, 7);
+            Console.WriteLine("Dice Value: " + rollDice);
+
+            Random num = new Random();
+            int value = num.Next(1, 4);
+            Console.WriteLine("case number is " + value);
+
+            if (value == 1)
+            {
+
+                Console.WriteLine("You got NO PLAY Option");
+
+            }
+            else if (value == 2)
+            {
+                Console.WriteLine("You got Ladder Option");
+
+                position = position + rollDice;
+
+                if (position > 100)
+                {
+
+                    position = position - rollDice;
+                    Console.WriteLine("You got Ladder");
+
+
+                }
+
+                Console.WriteLine(" your position: {0}", position);
+
+            }
+            else
+            {
+                position = position - rollDice;
+
+                Console.WriteLine("You got Snake Option");
+
+                if (position < 0)
+                {
+
+                    position = 0;
+
+                    Console.WriteLine("Starting game  again");
+
+
+                }
+                Console.WriteLine("You got snake \n your position: {0} \n ", position);
+
+            }
+        }
 
     }
 }
